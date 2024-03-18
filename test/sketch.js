bf3ecd080d7b9bedb4077029b5544ade7bf445b8
exuehn <149489839+exuehn@users.noreply.github.com>
2024-03-18 12:42:39 +0000

    test

--- test/sketch.js
+++ test/sketch.js
@@ -1,8 +1,8 @@
 const options = {
 
-  lat: 51.425222, //latitude
-  lng: -0.563167, //longitude
-  zoom: 3,
+  lat: 31.9522, //latitude
+  lng: 35.2332, //longitude
+  zoom: 7,
   style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
 
 }
@@ -10,6 +10,13 @@ const mappa = new Mappa('Leaflet')
 
 let myMap;
 let canvas;
+let dataset;
+
+function preload() {
+
+  dataset = loadTable("data.csv", "header");
+
+}
 
 function setup() {
 
@@ -24,10 +31,10 @@ function draw(){
 
   clear();
 
-  let pos = myMap.latLngToPixel(51.425222, -0.563167);
+  let pos = myMap.latLngToPixel(31.9522, 35.2332);
   fill(0);
   ellipse(pos.x, pos.y, 5, 5);
   textSize(20);
-  text("Royal Holloway", pos.x, pos.y-5);
+  text("Palestine", pos.x, pos.y-5);
 
 }
